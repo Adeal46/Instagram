63ed884b778b28946c29913e493abfb897b8ff22
Adeal <adililema3@gmail.com>
2019-03-15 23:25:00 +0300

    declared the allowed host

--- Instaclone/settings.py
+++ Instaclone/settings.py
@@ -55,7 +55,7 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS',cast=Csv())
 
 ACCOUNT_ACTIVATION_DAYS = 7
 
